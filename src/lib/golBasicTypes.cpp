cad8122b1b44fbeaff388ee171ad6e1168a7bdd9
Chongfeng Ling <ling.chongfeng@outlook.com>
2023-02-14 22:50:48 +0000

    added another constructor (randomly)

--- src/lib/golBasicTypes.cpp
+++ src/lib/golBasicTypes.cpp
@@ -20,6 +20,31 @@ namespace gol
     {
     }
 
+    Grid::Grid(int num_row, int num_col, int num_alive) : Grid(num_row, num_col)
+    {
+        if (num_row * num_col < num_alive)
+        {
+            throw std::invalid_argument("The number of alive cells exceed the total number of the grid.");
+        }
+        // set up random integer generator
+        std::mt19937 rng_mt;
+        std::uniform_int_distribution<> uniform_int_row(0, num_row - 1);
+        std::uniform_int_distribution<> uniform_int_col(0, num_col - 1);
+        // count for the number of alive at each stage
+        int counts = 0;
+        while (counts < num_alive)
+        {
+            int row = uniform_int_row(rng_mt);
+
+            int col = uniform_int_col(rng_mt);
+            if (!(cells[row][col]))
+            {
+                cells[row][col] = true;
+                counts++;
+            }
+        }
+    }
+
     void Grid::print()
     {
         for (int i = 0; i < row; ++i)
--- src/lib/golBasicTypes.h
+++ src/lib/golBasicTypes.h
@@ -43,6 +43,7 @@
 
 #include <iostream>
 #include <vector>
+#include <random>
 
 //! Single namespace for all code in this package
 namespace gol
@@ -53,6 +54,8 @@ namespace gol
     public:
         // constructor
         Grid(int row, int col);
+        // random constructor
+        Grid(int row, int col, int num_alive);
 
         void print();
         // get individual cell content
