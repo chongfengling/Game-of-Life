861473655dc613f40ddc387740ccf8182fe45b8e
Chongfeng Ling <ling.chongfeng@outlook.com>
2023-02-25 00:33:45 +0000

    specified two way to create a Grid and set an initial network of requirements

--- src/app/golSimulator.cpp
+++ src/app/golSimulator.cpp
@@ -9,6 +9,11 @@ int main(int argc, char **argv)
     // specify the version
     std::string gol_version{"0.1.0"};
     app.set_version_flag("--version", gol_version);
+    // specify the way to create a Grid: use file or use random
+    auto use_file = app.add_flag("-f,--file", "Create Grid from a file");
+    auto use_random = app.add_flag("-r,--random", "Create Grid randomly");
+    use_file->excludes(use_random);
+    use_random->excludes(use_file);
 
     CLI11_PARSE(app, argc, argv);
     return 0;
