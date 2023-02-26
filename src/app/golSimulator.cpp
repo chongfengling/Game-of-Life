1a9ec22c3e242489cdffb38b10db0bd7989b93ab
Chongfeng Ling <ling.chongfeng@outlook.com>
2023-02-26 13:50:42 +0000

    renamed opts and added type variables

--- src/app/golSimulator.cpp
+++ src/app/golSimulator.cpp
@@ -10,18 +10,20 @@ int main(int argc, char **argv)
     std::string gol_version{"0.1.0"};
     app.set_version_flag("--version", gol_version);
     // specify the way to create a Grid: use file or use random
-    auto use_file = app.add_flag("-f,--file", "Create Grid from a file");
-    auto use_random = app.add_flag("-r,--random", "Create Grid randomly");
-    use_file->excludes(use_random);
-    use_random->excludes(use_file);
+    bool use_file = false;
+    bool use_random = false;
+    auto use_file_opt = app.add_flag("-f,--file", use_file, "Create Grid from a file");
+    auto use_random_opt = app.add_flag("-r,--random", use_random, "Create Grid randomly");
+    use_file_opt->excludes(use_random_opt);
+    use_random_opt->excludes(use_file_opt);
     // specify the input file path
     std::string input_file;
-    app.add_option("-i,--input", input_file, "Specify the input file path")->check(CLI::ExistingFile)->needs(use_file)->excludes(use_random);
+    app.add_option("-i,--input", input_file, "Specify the input file path")->check(CLI::ExistingFile)->needs(use_file_opt)->excludes(use_random_opt);
     // specify the parameters for randomly creation
     int rows, cols, alive;
-    app.add_option("--rows", rows, "Number of rows for random initial values")->check(CLI::PositiveNumber)->needs(use_random)->excludes(use_file);
-    app.add_option("--cols", cols, "Number of columns for random initial values")->check(CLI::PositiveNumber)->needs(use_random)->excludes(use_file);
-    app.add_option("--alive", alive, "Number of initial alive cells for random initial values")->check(CLI::NonNegativeNumber)->needs(use_random)->excludes(use_file);
+    app.add_option("--rows", rows, "Number of rows for random initial values")->check(CLI::PositiveNumber)->needs(use_random_opt)->excludes(use_file_opt);
+    app.add_option("--cols", cols, "Number of columns for random initial values")->check(CLI::PositiveNumber)->needs(use_random_opt)->excludes(use_file_opt);
+    app.add_option("--alive", alive, "Number of initial alive cells for random initial values")->check(CLI::NonNegativeNumber)->needs(use_random_opt)->excludes(use_file_opt);
     // specify the number of generating steps
     int steps;
     app.add_option("--steps", steps, "Number of generations to simulate")->check(CLI::NonNegativeNumber)->required();
